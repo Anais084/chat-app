d7969c64572b657c8e35d635a5a85f8a5430ec6e
Anais <anais.chevallier@gmail.com>
2020-02-13 16:27:44 +0100

    implementing state

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import './Contact.css';
 
-const Contact = (props) => {
-  return (
-    <div className="Contact">
-    <img className="avatar" src={props.avatar} />
-    <div>
-    <h4 className="name">{props.name}</h4>
-    <div className="status">
-    <p className={props.online ? "status-online" : "status-offline"}></p>
-    <p className="status-text">{props.online ? 'Online' : 'Offline'}</p>
+
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      online: false
+    };
+  }
+  render() {
+    return <div className="Contact">
+      <img className="avatar" src={this.props.avatar} />
+      <div>
+        <h4 className="name">{this.props.name}</h4>
+        <div className="status"
+          onClick={event => {
+            const newState = !this.state.online
+            this.setState({ online: newState })
+          }}>
+          <p className={this.state.online ? "status-online" : "status-offline"}></p>
+          <p className="status-text">{this.state.online ? 'Online' : 'Offline'}</p>
+        </div>
+      </div>
     </div>
-    </div>
-  </div>
-  );
+  }
 }
 
 export default Contact;
